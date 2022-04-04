da9e4477397c0ef2539a08a9cac5725e269e8d56
Larissa Paulino <larissaspaulino@gmail.com>
2022-04-03 21:08:06 -0300

    feat: readme with projects description

--- src/components/ModalSeeTech/index.jsx
+++ src/components/ModalSeeTech/index.jsx
@@ -1,56 +1,63 @@
 import { StyledModal, Backdrop, styleBox } from './styles'
 import { Box } from '@mui/system'
 import { AiOutlineClose } from 'react-icons/ai'
-import Input from '../../components/Input'
-import Button from '../../components/Button'
-import Select from '../../components/Select'
+import Input from '../Input'
+import Button from '../Button'
+import Select from '../Select'
 import { grey1, primaryNegative } from '../../styles/global'
 import api from '../../services/api'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import { useState } from 'react'
 
-const ModalSeeTech = ({user, setUser, handleClose, open, techClicked }) => {
-
+const ModalSeeTech = ({ user, setUser, handleClose, open, techClicked }) => {
   const [token] = useState(localStorage.getItem('@hub:token'))
-  const [newStatus, setNewStatus] = useState("")
+  const [newStatus, setNewStatus] = useState('')
 
   const handleChange = () => {
-    
-    api.put(`/users/techs/${techClicked.id}`, newStatus, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }}
-    ).then(() => {
-      api.get(`/users/${user.id}`)
-      .then(res => {
-        localStorage.setItem('@hub:user', JSON.stringify(res.data))
-        setUser(res.data)
+    api
+      .put(`/users/techs/${techClicked.id}`, newStatus, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .catch(err => console.log('get by id:', err))
-      handleClose()
-      toast.success('Tech alterada com sucesso')
-    })
-    .catch(() => toast.error('Não foi possível alterar a tech, tente novamente'))
+      .then(() => {
+        api
+          .get(`/users/${user.id}`)
+          .then((res) => {
+            localStorage.setItem('@hub:user', JSON.stringify(res.data))
+            setUser(res.data)
+          })
+          .catch((err) => console.log('get by id:', err))
+        handleClose()
+        toast.success('Tech alterada com sucesso')
+      })
+      .catch(() =>
+        toast.error('Não foi possível alterar a tech, tente novamente')
+      )
   }
 
   const handleDelete = () => {
-    api.delete(`/users/techs/${techClicked.id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }}
-    ).then(() => {
-      api.get(`/users/${user.id}`)
-      .then(res => {
-        localStorage.setItem('@hub:user', JSON.stringify(res.data))
-        setUser(res.data)
+    api
+      .delete(`/users/techs/${techClicked.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .catch(err => console.log('get by id:', err))
-      handleClose()
-      toast.success('Tech excluída com sucesso')
-    })
-    .catch(() => toast.error('Não foi possível excluir a tech, tente novamente'))
+      .then(() => {
+        api
+          .get(`/users/${user.id}`)
+          .then((res) => {
+            localStorage.setItem('@hub:user', JSON.stringify(res.data))
+            setUser(res.data)
+          })
+          .catch((err) => console.log('get by id:', err))
+        handleClose()
+        toast.success('Tech excluída com sucesso')
+      })
+      .catch(() =>
+        toast.error('Não foi possível excluir a tech, tente novamente')
+      )
   }
-
 
   return (
     <div>
@@ -76,14 +83,14 @@ const ModalSeeTech = ({user, setUser, handleClose, open, techClicked }) => {
             <Select
               label='Selecionar módulo'
               name='status'
-              onChange={(e) => setNewStatus({status: e.target.value})}
-           >
+              onChange={(e) => setNewStatus({ status: e.target.value })}
+            >
               <option>Iniciante</option>
               <option>Intermediário</option>
               <option>Avançado</option>
             </Select>
             <div className='botoes'>
-              <Button bgColor={primaryNegative} onClick={handleChange} >
+              <Button bgColor={primaryNegative} onClick={handleChange}>
                 Salvar alterações
               </Button>
               <Button bgColor={grey1} onClick={handleDelete}>
